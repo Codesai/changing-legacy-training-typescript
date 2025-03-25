491525e926e443daed9140ed58662575d2f8e674
trikitrok <trikitrok@gmail.com>
2025-03-25 21:25:37 +0100

    adding new kata

---
+++ dependency-breaking-techniques/interactive-checkout/tests/Checkout.test.ts
@@ -0,0 +1,20 @@
+import {Product} from "../src/Product";
+import {EmailService} from "../src/EmailService";
+import {Checkout} from "../src/CheckOut";
+
+describe("Checkout", () => {
+    it("order comfirmation falis if user does not accept terms of service", () => {
+        // note for tester:
+        // Accept Newsletter
+        // Do not Accept Terms
+        const emailService: jest.Mocked<EmailService> = {
+            subscribeUserFor: jest.fn(),
+        };
+        const polkaDotSocks = new Product("Polka-dot Socks");
+        const checkout = new Checkout(polkaDotSocks, emailService);
+
+        expect(() => {
+            checkout.confirmOrder()
+        }).toThrow();
+    });
+});
