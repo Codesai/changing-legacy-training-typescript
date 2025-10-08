0016b53ff168eb3e93a2632e29e7c82427917c02
freyes <fran@codesai.com>
2025-10-08 16:05:31 +0100

    resetting config reporters to default after the tests

--- gilded_rose/test/helpers/approvalVerifiers.ts
+++ gilded_rose/test/helpers/approvalVerifiers.ts
@@ -1,25 +1,35 @@
 import {WebStormReporter} from "./webStormReporter";
 import {configure} from "approvals/lib/Approvals";
 import {
-    verifyAllCombinations as approvalsVerifyAllCombinations, Printer, VariationsForEachParameter
+    Printer,
+    VariationsForEachParameter,
+    verifyAllCombinations as approvalsVerifyAllCombinations
 } from "approvals/lib/Providers/Jest/CombinationApprovals";
-import {defaultConfig} from "approvals/lib/config";
-
-let reporterConfigured = false;
+import {currentConfig} from "approvals/lib/config";
 
 export function verifyAllCombinations<T extends any[]>(
     func: Printer<T>,
     ...variations: VariationsForEachParameter<T>) {
-    if (!reporterConfigured) {
-        configureWebStormReporter();
-    }
-    approvalsVerifyAllCombinations(func, ...variations)
+    configureWebStormReporter();
+    approvalsVerifyAllCombinations(func, ...variations);
+    resetConfiguration()
 }
 
 function configureWebStormReporter() {
-    const currentConfig = JSON.parse(JSON.stringify(defaultConfig));
+    const currentConfig = getCurrentConfig();
     currentConfig.reporters = [
         new WebStormReporter(),
+        ...currentConfig.reporters,
     ];
     configure(currentConfig);
+}
+
+function resetConfiguration() {
+    const currentConfig = getCurrentConfig();
+    currentConfig.reporters = currentConfig.reporters.slice(1);
+    configure(currentConfig);
+}
+
+function getCurrentConfig(){
+    return JSON.parse(JSON.stringify(currentConfig()));
 }
